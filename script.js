0870f1219d35b0cc331cab7f1649d8a732398931
Elchin Mammadli <elchinmammadli052@gmail.com>
2022-10-19 17:00:16 +0400

    Inheritance implemented

--- script.js
+++ script.js
@@ -11,8 +11,54 @@ const inputElevation = document.querySelector('.form__input--elevation');
 const formInputs = document.querySelectorAll('.form__input');
 // GeoLocation API
 const myGeolocation = navigator.geolocation;
-// Functions
-// Reset the form values and hide form
+////////////////////////////////
+// PARENT WORKPUT CLASS
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-5);
+  constructor(coords, distance, duration) {
+    // this.date = date;
+    // this.id = id;
+    this.coords = coords; // [lat, lng]
+    this.duration = duration; // in min
+    this.distance = distance; // in km
+  }
+}
+
+///////////////////////////////////////
+// CHILD CLASSES
+class Running extends Workout {
+  constructor(coords, distance, duration, cadance) {
+    super(coords, distance, duration);
+    this.cadance = cadance;
+    this.calcPace();
+  }
+
+  calcPace() {
+    // min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+  calcSpeed() {
+    // km/h
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+const run1 = new Running([39, -12], 5.2, 24, 170);
+const cycle1 = new Cycling([39, -12], 27, 95, 523);
+console.log(run1, cycle1);
+///////////////////////////////////
+// APPLICATION ARCHITECTURE
 class App {
   // private instance properties
   #map;
